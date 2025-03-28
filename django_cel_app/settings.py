5e5deeb6a442f9b57b4f1941708d4b91ed880a6c
Vishal Kaushik <vishalkaushik2002@gmail.com>
2025-03-28 21:36:40 +0530

    Updated django_cel_app/settings.py

--- django_cel_app/settings.py
+++ django_cel_app/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#4b#gi1%t!wx06+7q)l506h@xfp#(cjf#1l%#k=d!$8q63q8om'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
